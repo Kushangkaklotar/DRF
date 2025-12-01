b6f7d9776f88d1fc28047755e8218d0bbf932c19
kushang-iroid <kushangkaklotar@gmail.com>
2025-12-01 12:49:27 +0530

    create :- Simple home page for display details

--- DRF/DRF/urls.py
+++ DRF/DRF/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from home.views import *
 
 urlpatterns = [
-    path('', ),
+    path('', home_page),
     path('api/', include('api.urls')),
     path('admin/', admin.site.urls),
 ]
--- DRF/home/views.py
+++ DRF/home/views.py
@@ -1,6 +1,11 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from django.shortcuts import render
+
 from home.serializers import *
+
+def home_page(request):
+    return render(request, "home.html")
 
 @api_view(['GET', 'POST'])
 def hello_world(request):
