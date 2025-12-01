d9ddfd5066eb90894be4991068a33378817a3c51
kushang-iroid <kushangkaklotar@gmail.com>
2025-12-01 16:14:55 +0530

    create :- Add some changes for deploy

--- DRF/DRF/settings.py
+++ DRF/DRF/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-gr4__!7)q3r1+uagwdps-e-7cblm)l1b2x60t(1#e7a+!aav$s
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
